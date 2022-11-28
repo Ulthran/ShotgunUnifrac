5b933929e9e9d6cc0e7b8f9a3460e2637edac59b
ctbus <ctbushman@gmail.com>
2022-11-28 11:37:37 -0500

    Test with singularity

--- .tests/integration/test_full_supermatrix_run.py
+++ .tests/integration/test_full_supermatrix_run.py
@@ -28,6 +28,7 @@ def test_full_run():
             "all",
             "-c",
             "--use-conda",
+            "--use-singularity",
             "--conda-prefix", ".snakemake/",
             "--directory", tmpdir,
         ])
