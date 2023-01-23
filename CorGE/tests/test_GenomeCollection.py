104706252ba6e18164663193f5901a20c40f7f65
ctbus <ctbushman@gmail.com>
2023-01-23 16:52:07 -0500

    Re-add assembly_summary for tests

--- CorGE/tests/test_GenomeCollection.py
+++ CorGE/tests/test_GenomeCollection.py
@@ -1,13 +1,49 @@
+import os
 import pytest
 from src.CorGE.GenomeCollection import GenomeCollection
+
+OUTPUT_FP = os.path.join(os.getcwd(), "output/")
 
 
 @pytest.fixture
 def genome_collection():
     yield GenomeCollection()
 
+
 def test_genome_collection(genome_collection):
     gc = genome_collection
     gc.dryrun()
     gc.collect()
 
+
+@pytest.fixture
+def genome_collection_species():
+    yield GenomeCollection(OUTPUT_FP, [2173])
+
+
+def test_genome_collection(genome_collection):
+    gc = genome_collection
+    gc.dryrun()
+    gc.collect()
+
+
+@pytest.fixture
+def genome_collection_accessions():
+    yield GenomeCollection(OUTPUT_FP, ["GCF_000007725.1"])
+
+
+def test_genome_collection(genome_collection):
+    gc = genome_collection
+    gc.dryrun()
+    gc.collect()
+
+
+@pytest.fixture
+def genome_collection_local():
+    yield GenomeCollection(OUTPUT_FP, "test-data/collected-genomes")
+
+
+def test_genome_collection(genome_collection):
+    gc = genome_collection
+    gc.dryrun()
+    gc.collect()
