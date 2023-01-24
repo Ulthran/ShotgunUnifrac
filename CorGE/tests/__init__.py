f6e88e45173379b8a931571105ba8c9f40c021ee
ctbus <ctbushman@gmail.com>
2023-01-24 11:39:28 -0500

    Fix GenomeCollection tests

--- CorGE/tests/__init__.py
+++ CorGE/tests/__init__.py
@@ -1,4 +1,8 @@
 import os
+import tempfile
 
-OUTPUT_FP = os.path.join(os.getcwd(), "output/")
+TESTS_FP = os.path.dirname(os.path.realpath(__file__))
+TEST_DATA_FP = os.path.join(TESTS_FP, "test-data")
+TEMP_FP = tempfile.mkdtemp()
+OUTPUT_FP = os.path.join(TEMP_FP, "output/")
 GENOMES_FP = os.path.join(OUTPUT_FP, "genomes/")
--- CorGE/tests/test_GenomeCollection.py
+++ CorGE/tests/test_GenomeCollection.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import shutil
 from src.CorGE.GenomeCollection import GenomeCollection
 from . import *
 
@@ -7,6 +8,7 @@ from . import *
 @pytest.fixture
 def genome_collection():
     yield GenomeCollection(OUTPUT_FP)
+    shutil.rmtree(OUTPUT_FP)
 
 
 def test_genome_collection(genome_collection):
@@ -14,12 +16,13 @@ def test_genome_collection(genome_collection):
     gc.dryrun()
     gc.collect()
 
-    assert os.listdir(GENOMES_FP) == []
+    assert set(os.listdir(GENOMES_FP)) == set([])
 
 
 @pytest.fixture
 def genome_collection_species():
-    yield GenomeCollection(OUTPUT_FP, [2173])
+    yield GenomeCollection(OUTPUT_FP, ["2173"])
+    shutil.rmtree(OUTPUT_FP)
 
 
 def test_genome_collection_species(genome_collection_species):
@@ -27,12 +30,15 @@ def test_genome_collection_species(genome_collection_species):
     gc.dryrun()
     gc.collect()
 
-    assert set(os.listdir(GENOMES_FP)) == set(["GCF_000016525.1.fna", "GCF_000016525.1.faa"])
+    assert set(os.listdir(GENOMES_FP)) == set(
+        ["GCF_000016525.1.fna", "GCF_000016525.1.faa"]
+    )
 
 
 @pytest.fixture
 def genome_collection_accessions():
-    yield GenomeCollection(OUTPUT_FP, ["GCF_000007725.1"])
+    yield GenomeCollection(OUTPUT_FP, [], ["GCF_000007725.1"])
+    shutil.rmtree(OUTPUT_FP)
 
 
 def test_genome_collection_accessions(genome_collection_accessions):
@@ -40,12 +46,17 @@ def test_genome_collection_accessions(genome_collection_accessions):
     gc.dryrun()
     gc.collect()
 
-    assert set(os.listdir(GENOMES_FP)) == set(["GCF_000007725.1.fna", "GCF_000007725.1.faa"])
+    assert set(os.listdir(GENOMES_FP)) == set(
+        ["GCF_000007725.1.fna", "GCF_000007725.1.faa"]
+    )
 
 
 @pytest.fixture
 def genome_collection_local():
-    yield GenomeCollection(OUTPUT_FP, "test-data/collected-genomes")
+    yield GenomeCollection(
+        OUTPUT_FP, [], [], os.path.join(TEST_DATA_FP, "collected-genomes")
+    )
+    shutil.rmtree(OUTPUT_FP)
 
 
 def test_genome_collection_local(genome_collection_local):
@@ -53,4 +64,31 @@ def test_genome_collection_local(genome_collection_local):
     gc.dryrun()
     gc.collect()
 
-    assert set(os.listdir(GENOMES_FP)) == set(["GCF_000016525.1.fna", "GCF_000016525.1.faa"])
+    assert set(os.listdir(GENOMES_FP)) == set(
+        [
+            "GCF_000007725.1.faa",
+            "GCF_000007725.1.fna",
+            "GCF_000010525.1.faa",
+            "GCF_000010525.1.fna",
+            "GCF_000012885.1.faa",
+            "GCF_000012885.1.fna",
+            "GCF_000016525.1.faa",
+            "GCF_000016525.1.fna",
+            "GCF_000020965.1.faa",
+            "GCF_000020965.1.fna",
+            "GCF_000218545.1.faa",
+            "GCF_000218545.1.fna",
+            "GCF_000378225.1.faa",
+            "GCF_000378225.1.fna",
+            "GCF_001375595.1.faa",
+            "GCF_001375595.1.fna",
+            "GCF_001735525.1.faa",
+            "GCF_001735525.1.fna",
+            "GCF_007197645.1.faa",
+            "GCF_007197645.1.fna",
+            "GCF_023159115.1.faa",
+            "GCF_023159115.1.fna",
+            "GCF_900111765.1.faa",
+            "GCF_900111765.1.fna",
+        ]
+    )
