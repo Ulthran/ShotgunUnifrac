9d6465e316e1245f3e24cfd44268110030b6d4d2
ctbus <ctbushman@gmail.com>
2023-01-24 13:58:23 -0500

    Reformat

--- .tests/integration/test_full_nucl_run.py
+++ .tests/integration/test_full_nucl_run.py
@@ -28,7 +28,7 @@ def test_full_run():
             os.path.join(tmpdir, "merged-sequences/"),
         )
 
-        with open(os.path.join(tmpdir, "config.yml"), 'a') as f:
+        with open(os.path.join(tmpdir, "config.yml"), "a") as f:
             f.write(f"\nDATA: {tmpdir}")
 
         os.system("conda config --set channel_priority strict")
